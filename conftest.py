0b77f823f5e60b1b42af83beff6806758a3850c6
GatskevichVika <gatskevich.vika@gmail.com>
2021-05-19 11:40:35 +0300

    Добавлен тест создания группы

--- conftest.py
+++ conftest.py
@@ -1,7 +1,7 @@
 import pytest
 from fixture.application import Application
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def app(request):
     fixture = Application("C:\\Tools\\AddressBook.exe")
     request.addfinalizer(fixture.destroy)
